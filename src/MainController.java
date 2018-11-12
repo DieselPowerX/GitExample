a8c3e43186fcc9beaa32270bf2730bb787cbd47e
Konrad Bobowski <konrad.bobowski92@gmail.com>
2018-11-12 10:13:09 +0100

    add method in main

--- src/MainController.java
+++ src/MainController.java
@@ -2,4 +2,7 @@ public class MainController {
     public static void main(String[] args) {
         System.out.println("wellcome to the git");
     }
+    public static void printSomeText(){
+        System.out.println();
+    }
 }
