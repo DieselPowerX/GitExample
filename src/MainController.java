39b5da6767067537ab6d01d4167f7526852d3ce8
Konrad Bobowski <konrad.bobowski92@gmail.com>
2018-11-12 12:22:53 +0100

    changed url

--- src/MainController.java
+++ src/MainController.java
@@ -4,7 +4,7 @@ import java.net.URL;
 public class MainController {
     public static void main(String[] args) throws Exception {
 
-        String url  ="http://www.google.pl";
+        String url  ="http://www.google.de";
         URL conection =  new URL(url);
         HttpURLConnection con = (HttpURLConnection) conection.openConnection();
         
