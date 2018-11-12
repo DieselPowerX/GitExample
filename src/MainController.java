287110a1877a2d0d37b4e60b30c4d79a00ab9183
Konrad Bobowski <konrad.bobowski92@gmail.com>
2018-11-12 13:37:25 +0100

    Cos

--- src/MainController.java
+++ src/MainController.java
@@ -7,6 +7,8 @@ public class MainController {
     public static void main(String[] args) throws Exception {
 
 
+        /// to jest moja galaz 
+
 
         String url  ="http://www.apple.de";
         
