8d718e70d9b8cee8944b443b0b2346cf6a837db2
Dawid Jurkowski <dawid.jurkowski.ak@gmail.com>
2018-11-12 12:24:26 +0100

    new massage xd

--- src/MainController.java
+++ src/MainController.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -7,6 +9,15 @@ public class MainController {
         String url  ="http://www.google.pl";
         URL conection =  new URL(url);
         HttpURLConnection con = (HttpURLConnection) conection.openConnection();
+
+        con.setRequestMethod("GET");
+
+        int responseCode = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
         
 
     }
